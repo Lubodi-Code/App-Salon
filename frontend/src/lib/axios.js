49dc2742d95b81eb96b03b9e7bce29631e92f331
Lubodi-Code <luisfelipebolanosdixon@gmail.com>
2025-06-06 17:03:21 -0600

    Configuración de axios con interceptor para manejar token de autenticación

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/", // Se utiliza la variable de entorno
+  baseURL: import.meta.env.VITE_API_URL, // Se utiliza la variable de entorno
   headers: { 'Content-Type': 'application/json' }
 });
 
